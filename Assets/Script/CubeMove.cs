84bc5a16cb4e1a81bfd7d22ffb321f45011c7bef
TSITCL\s162034 <elitesr2110@gmail.com>
2017-04-21 16:53:56 +0900

    プロジェクトの追加

---
+++ Assets/Script/CubeMove.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour
+{
+
+    public Vector3 StartPos;        // 初期位置
+    public Vector3 EndPos;          // 終了位置
+    public float time;              // 時間
+
+    private Vector3 deltaPos;       
+    private float elapsedTime;
+    private bool bStartToEnd = true;
+
+    void Start()
+    {
+        // StartPosをオブジェクトに初期位置に設定
+        transform.position = StartPos;
+
+        // 1秒当たりの移動量を算出
+        deltaPos = (EndPos - StartPos) / time;
+        elapsedTime = 0;
+    }
+
+    void Update()
+    {
+        // 1秒当たりの移動量にTime.deltaTimeを掛けると1フレーム当たりの移動量となる
+        // Time.deltaTimeは前回Updateが呼ばれてからの経過時間
+        transform.position += deltaPos * Time.deltaTime;
+
+        // 往路復路反転用経過時間
+        elapsedTime += Time.deltaTime;
+
+        // 移動開始してからの経過時間がtimeを超えると往路復路反転
+        if (elapsedTime > time)
+        {
+            if (bStartToEnd)
+            {
+                // StartPos→EndPosだったので反転してEndPos→StartPosにする
+                // 現在の位置がEndPosなので StartPos - EndPosでEndPos→StartPosの移動量になる
+                deltaPos = (StartPos - EndPos) / time;
+                
+                // 誤差があるとずれる可能性があるので念のためオブジェクトの位置をEndPosに設定
+                transform.position = EndPos;
+            }
+            else
+            {
+                // EndPos→StartPosだったので反転してにStartPos→EndPosする
+                // 現在の位置がStartPosなので EndPos - StartPosでStartPos→EndPosの移動量になる
+                deltaPos = (EndPos - StartPos) / time;
+            
+                // 誤差があるとずれる可能性があるので念のためオブジェクトの位置をSrartPosに設定
+                transform.position = StartPos;
+            }
+            // 往路復路のフラグ反転
+            bStartToEnd = !bStartToEnd;
+
+            // 往路復路反転用経過時間クリア
+            elapsedTime = 0;
+        }
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    public float pos_x = 4.65f;
+    public float pos_y = 5.47f;
+    public float pos_z;
+
+    GameObject Player;
+    GameObject mainCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        mainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mainCamera.transform.position = new Vector3(pos_x, pos_y, Player.transform.position.z - 10);
+
+    }
+}
