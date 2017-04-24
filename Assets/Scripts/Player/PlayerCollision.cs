72ae65983dfc330d0b943101ea9ec9ed95af117d
hetima333 <hetima333@gmail.com>
2017-04-24 16:06:58 +0900

    [WIP]プレイヤーの衝突スクリプトの追加(判定はまだ取れない)

---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------
+// Brief : プレイヤーの当たり判定
+// 
+// Date  : 2014/04/24
+// 
+// Author: Y.Watanabe
+// ---------------------------------------
+
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour
+{
+	private Collider[] m_colliders;
+
+
+	// はさまれているか
+	private int m_isInterPose;
+
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start ()
+	{
+		// Colliderの参照
+		m_colliders = GetComponentsInChildren<Collider>();
+
+		foreach (var item in m_colliders)
+		{
+			item.OnTriggerEnterAsObservable()
+				.Where(col => col != transform.root.GetComponent<Collider>())
+				.Subscribe(_ => {
+					m_isInterPose++;
+					Debug.Log(_.transform.name);
+				 });
+
+			item.OnTriggerExitAsObservable()
+				.Where(col => col != transform.root.GetComponent<Collider>())
+				.Subscribe(_ => {
+					m_isInterPose--;
+					Debug.Log(_.transform.name);
+				});
+
+		}
+
+		this.UpdateAsObservable()
+			.Subscribe(_ => Debug.Log(m_isInterPose));
+	}
+}
