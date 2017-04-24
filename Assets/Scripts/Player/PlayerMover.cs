dbe86bada25a97dfba39b91d7720fef08a5246cb
hetima333 <hetima333@gmail.com>
2017-04-24 12:08:59 +0900

    プロジェクトの追加

---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------
+// Brief : プレイヤーの移動
+// 
+// Date  : 2017/04/24
+// 
+// Author: Y.Watanabe
+// ---------------------------------------
+
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMover : MonoBehaviour
+{
+	[SerializeField, Range(0, 10)]
+	private float m_speed;
+
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start ()
+	{
+		this.UpdateAsObservable()
+			.Where(_ => Input.GetKey(KeyCode.Space))
+			.Subscribe(_ =>
+			{
+				Debug.Log("Pressed Space");
+				transform.position += new Vector3(0, 0, m_speed) * Time.deltaTime;
+			});
+	}
+}
---
+++ Assets/Scripts/Stage/GimmickMover.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------
+// Brief : ギミックの移動
+// 
+// Date  : 2017/04/24
+// 
+// Author: Y.Watanabe
+// ---------------------------------------
+
+using UniRx;
+using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GimmickMover : MonoBehaviour
+{
+	[SerializeField]
+	private float m_targetPosition;
+
+	// private 
+
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start ()
+	{
+		// transform.DOLocalMoveX(m_targetPosition, )
+	}
+}
