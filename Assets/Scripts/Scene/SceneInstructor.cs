d305bc236d4e3c778b8ba5da2cada736c71d7717
TSITCL\s162196 <dqx_2527@yahoo.co.jp>
2017-04-24 18:07:57 +0900

    Scene遷移周り作成

---
+++ Assets/Scripts/Scene/SceneInstructor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //シーンの管理用
+
+// 山元のやーつ
+namespace YamagenLib
+{
+    // シーンのの列挙(シーン名と同じ)
+    public enum GameScene
+    {
+        Test,
+        Title,
+        Select,
+        Play,
+    }
+
+    // シーンに指示出すクラス
+    public class SceneInstructor : MonoBehaviour
+    {
+        [SerializeField]
+        // 初期シーン
+        GameScene m_initScene=GameScene.Test;
+
+        // ロードされてるシーン
+        Scene m_loadScene;
+
+        bool m_initFlag = true;
+
+        // 初期化
+        void Awake()
+        {
+            // 初期シーンをロード
+            LoadMainScene(m_initScene);
+        }
+
+        // メインのシーンをロードする
+        public void LoadMainScene(GameScene scene)
+        {
+            if (m_initFlag == false)
+                SceneManager.UnloadSceneAsync(m_loadScene.name);
+            else
+                m_initFlag = false;
+            SceneManager.LoadScene(scene.ToString(), LoadSceneMode.Additive);
+            m_loadScene = SceneManager.GetSceneByName(scene.ToString());
+
+        }
+    }
+}
