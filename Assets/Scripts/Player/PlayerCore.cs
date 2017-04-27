ad5cdb820f6edbc5820e2dd988b001fbc0591ee2
hetima333 <hetima333@gmail.com>
2017-04-27 14:09:48 +0900

    PlayerCoreの追加

---
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------
+// Brief : プレイヤーのコア部分
+// 
+// Date  : 2017/04/27
+// 
+// Author: Y.Watanabe
+// ---------------------------------------
+
+using UniRx;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCore : MonoBehaviour
+{
+	[SerializeField, Range(0, 10), Tooltip("登場にかかる時間")]
+	private float m_waitTime;
+
+	/// <summary>
+	/// プレイヤが操作可能な状態か
+	/// </summary>
+	public ReactiveProperty<bool> PlayerControllable = new BoolReactiveProperty(false);
+
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start ()
+	{
+		StartCoroutine(WaitStartAnimation());
+	}
+
+
+	/// <summary>
+	/// 登場演出で少し待機する
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator WaitStartAnimation ()
+	{
+		yield return new WaitForSeconds(m_waitTime);
+
+		PlayerControllable.Value = true;
+	}
+}
