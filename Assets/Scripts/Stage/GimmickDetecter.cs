766a8f7bf2aa8b5ecefb32ddcd5627c51767611d
hetima333 <hetima333@gmail.com>
2017-05-08 14:10:55 +0900

    [WIP]判定付きギミック作成

---
+++ Assets/Scripts/Stage/GimmickDetecter.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------
+// Brief : 判定付きギミック
+// 
+// Date  : 2017/05/08
+// 
+// Author: Y.Watanabe
+// ---------------------------------------
+
+using UniRx;
+using UnityEngine;
+using System.Collections;
+
+public class GimmickDetecter : MonoBehaviour
+{
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start ()
+	{
+	
+	}
+}
