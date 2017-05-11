b036f7047374bb156f2eac725fed983bf342f4e8
TSITCL\s162034 <elitesr2110@gmail.com>
2017-05-11 18:32:22 +0900

    プロジェクト更新

---
+++ Assets/Scripts/Player/BreakBlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space))
+            Breack();
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.GetInstanceID() == transform.GetInstanceID())
+            return;
+
+        Debug.Log("hit");
+
+    }
+
+    void Breack()
+    {
+        var parts = GetComponentsInChildren<Transform>();
+
+        foreach (var item in parts)
+        {
+            item.gameObject.AddComponent<Rigidbody>();
+        }
+
+    }
+}
