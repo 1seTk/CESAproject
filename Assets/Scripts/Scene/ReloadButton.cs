767d35e87ee469b912de2dfea38af61a422c9f6f
TSITCL\s162196 <dqx_2527@yahoo.co.jp>
2017-05-15 17:12:32 +0900

    プレイシーンのマルチシーン化とリロード追加

---
+++ Assets/Scripts/Scene/ReloadButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   // ボタン用
+using UnityEngine.SceneManagement;  //シーンの管理用
+
+// 山元のやーつ
+namespace YamagenLib
+{
+    // ステージリロードのボタン用クラス
+    public class ReloadButton : MonoBehaviour
+    {
+        // 初期化
+        void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(ReLoadScene);
+        }
+
+        // シーンをリロードする
+        public void ReLoadScene()
+        {
+            FindObjectOfType<PlayInstructor>().ReLoadStage();
+        }
+    }
+}
