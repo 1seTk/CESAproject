a80a9e5b291d9a9ae46f62c3a802cf965f8aa9e5
shun9 <hanashun9@gmail.com>
2017-05-26 19:02:45 +0900

    時間計測クラス作成

---
+++ Assets/Scripts/Clear/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // 経過時間
+    private float m_elapsedTime = 0.0f;
+
+    void Start ()
+    {
+
+    }
+
+    // 更新処理
+    void Update ()
+    {
+        m_elapsedTime += Time.deltaTime;
+        Debug.Log(m_elapsedTime);
+	}
+
+
+    /// <summary>
+    /// 経過時間をリセット
+    /// </summary>
+    void Reset()
+    {
+        m_elapsedTime = 0.0f;
+    }
+
+    float GetTime()
+    {
+        return m_elapsedTime;
+    }
+}
