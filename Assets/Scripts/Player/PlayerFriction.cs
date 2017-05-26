b51d0d6efc6ea69eebd8a49238c40dd6acf03067
hetima333 <hetima333@gmail.com>
2017-05-26 16:23:13 +0900

    [WIP]レイのテスト

--- Assets/Scripts/Player/PlayerFriction.cs
+++ Assets/Scripts/Player/PlayerFriction.cs
@@ -17,9 +17,13 @@ public class PlayerFriction : MonoBehaviour
 	[SerializeField, Range(0, 10)]
 	private float m_hitDistance;
 
+	[SerializeField]
+	private LayerMask m_ignoreMask;
 
 	void Start ()
 	{
+		RaycastHit hit = new RaycastHit();
+
 		// 四方にレイを飛ばす
 
 		// レイにヒットしたオブジェクトを方向のリストに
@@ -47,90 +51,19 @@ public class PlayerFriction : MonoBehaviour
 		//		Debug.Log(m_hitObject.name);
 		//	});
 
-		RaycastHit p = Physics.Raycast(transform.position, transform.forward, out hit, m_hitDistance);
 		// 衝突情報と通知を飛ばす
 		this.UpdateAsObservable()
-			.Select(_ => p)
-			.Subscribe(x =>
-			{
-				isHitRP.SetValueAndForceNotify(x);
-				m_hitObject = hit.transform;
-				Debug.Log(m_hitObject.name);
+			.Where(_ => Physics.Raycast(transform.position, -transform.up, out hit, m_hitDistance, ~m_ignoreMask) == true)
+			.Subscribe(x => {
+				//isHitRP.SetValueAndForceNotify(x);
+				//m_hitObject = hit.transform;
+				//Debug.Log(m_hitObject.name);
+				Debug.Log("hit");
 			});
 
-	}
-}
 
-public class RayTest : MonoBehaviour
-{
-
-	public LayerMask mask;
-
-	void Ray ()
-	{
-
-		// 1.
-		// Rayの作成
-		Ray ray = new Ray(transform.position, transform.forward);
-		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-		// 2.		
-		// Rayが衝突したコライダーの情報を得る
-		RaycastHit hit;
-		// Rayが衝突したかどうか
-		if (Physics.Raycast(ray, out hit, 10.0f, mask))
-		{
-			// Examples
-			// 衝突したオブジェクトの色を赤に変える
-			hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
-			// 衝突したオブジェクトを消す
-			Destroy(hit.collider.gameObject);
-			// Rayの衝突地点に、このスクリプトがアタッチされているオブジェクトを移動させる
-			this.transform.position = hit.point;
-			// Rayの原点から衝突地点までの距離を得る
-			float dis = hit.distance;
-			// 衝突したオブジェクトのコライダーを非アクティブにする
-			hit.collider.enabled = false;
-		}
-
-		// 3.		
-		// Rayが衝突した全てのコライダーの情報を得る。＊順序は保証されない
-		RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity);
-		foreach (var obj in hits)
-		{
-			// 衝突したオブジェクトのタグ毎に色を変え、当てはまらない場合はオブジェクトを非アクティブにする
-			switch (obj.collider.tag)
-			{
-				case "Red":
-					obj.collider.GetComponent<MeshRenderer>().material.color = Color.red;
-					break;
-				case "Blue":
-					obj.collider.GetComponent<MeshRenderer>().material.color = Color.blue;
-					break;
-				case "Green":
-					obj.collider.GetComponent<MeshRenderer>().material.color = Color.green;
-					break;
-				default:
-					obj.collider.gameObject.SetActive(false);
-					break;
-			}
-		}
-
-		// 4.		
-		// 球形のRayをとばす
-		if (Physics.SphereCast(ray, 5.0f, out hit, 10.0f)) { }
-
-		// 5.		
-		// Rayの可視化
-		Debug.DrawRay(ray.origin, ray.direction, Color.red, 3.0f);
-
-		// 6.		
-		// マウスがEventSystem上にあるか
-		UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
-		// マウスがEventSystem上にないときに、左クリックした場合
-		if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
-		{
-			// 処理内容
-		}
+		// デバッグRay描画
+		this.UpdateAsObservable()
+			.Subscribe(_ => Debug.DrawRay(transform.position, -transform.up, Color.red, m_hitDistance));
 	}
 }
