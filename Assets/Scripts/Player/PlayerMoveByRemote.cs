603c8397931cb5805634e7e9231994cda43c7aff
mokopo <mkp0824@i.softbank.jp>
2017-06-05 15:11:46 +0900

    プレイヤーがフリック動作中にタップされた場合その場に落下していくように修正

--- Assets/Scripts/Player/PlayerMoveByRemote.cs
+++ Assets/Scripts/Player/PlayerMoveByRemote.cs
@@ -10,6 +10,10 @@ using System;
 
 public class PlayerMoveByRemote : MonoBehaviour, IPlayerInput
 {
+    // 定数
+    private const int DELAY_JUMP_FRAME = 5;         // ジャンプ許容値
+    private const float ACCEPT_ABLE_VALUE = 30.0f;  // スワイプ許容値
+
     // タッチ開始座標
     private float _touchStartPosY;
 
@@ -20,12 +24,9 @@ public class PlayerMoveByRemote : MonoBehaviour, IPlayerInput
 
     private bool _jumpFlg = false;
 
-    private bool _flickFlg = false;
-
     private bool _advanceFlg = false;
 
-    [Range(5,100)]
-    public int _delayFrame = 20;
+    private bool _jumpMoveFlg = false;
 
     private CheckGround _checkGround;
 
@@ -85,84 +86,173 @@ public class PlayerMoveByRemote : MonoBehaviour, IPlayerInput
     /// </summary>
     private void Update()
     {
-        foreach (Touch t in Input.touches)
+        // フリック後に着地したなら
+        if (_jumpMoveFlg && _checkGround.IsGround.Value)
         {
-            switch (t.phase)
+            _jumpMoveFlg = false;
+
+            MoveStop();
+        }
+
+        // 押されたとき
+        if (Input.GetMouseButtonDown(0)){
+            // タッチ開始座標
+            _touchStartPosY = Input.mousePosition.y;
+
+            if (!_checkGround.IsGround.Value)
             {
-                case TouchPhase.Began:
-                    // タッチ開始座標を代入
-                    _touchStartPosY = t.position.y;
-
-                    Debug.Log("Begin");
-                    break;
-                case TouchPhase.Stationary:
-                    // タッチ時間計測を開始
-                    _touchTime++;
-                    Debug.Log("Stationaly");
-                    
-                    // タッチ時間が一定フレームを超えたら前進開始
-                    if (_touchTime > _delayFrame /*&& _checkGround.IsGround.Value == true*/)
-                    {
-                        _advanceFlg = true;
-                    }
-                    break;
-                case TouchPhase.Ended:
-                    // タッチ終了座標を代入
-                    _touchEndPosY = t.position.y;
-
-                    Debug.Log("StartPos" + _touchStartPosY);
-
-                    Debug.Log("EndPos" + _touchEndPosY);
-
-                    // 移動していない場合ジャンプを実行
-                    if (_advanceFlg == false)
-                    {                        
-                        _jumpFlg = true;
-                        Observable.NextFrame().Subscribe(_ =>
-                        {
-                            _jumpFlg = false;
-                        });
-                    }
-                    
-                    // 指が離れた時に前進移動を停止
-                    if ((_advanceFlg == true && _checkGround.IsGround.Value) == true)
-                    {
-                        _advanceFlg = false;
-                    }
-
-                    if (30 < Math.Abs(_touchEndPosY) - Mathf.Abs(_touchStartPosY))
-                    {
-                        // スワイプしている場合
-                        _advanceFlg = true;
-                        if ((_checkGround.IsGround.Value) == true)
-                        {
-                            transform.DOMoveZ(transform.position.z + _adPower, _jumpTime);
-                            transform.DOMoveY(transform.position.y + 5, _jumpTime);
-
-                            //_advanceFlg = true;
-
-                            //_jumpFlg = true;
-                            //Observable.NextFrame().Subscribe(_ =>
-                            //{
-                            //    _jumpFlg = false;
-
-                            //    //_advanceFlg = false;
-                            //});
-
-
-                        }
-                    }
-                    // タッチ時間をリセット
-                    _touchTime = 0;
-                    break;
+                if (_advanceFlg && _jumpFlg)
+                {
+                    MoveStop();
+                }
             }
         }
-        if (_checkGround.IsGround.Value != _oldGroundValue)
-        {
-            _advanceFlg = false;
+
+        // 押されている間
+        if (Input.GetMouseButton(0)){
+            // タッチフレームをカウント
+            _touchTime++;
+
+            // タッチフレームが許容値を超えたら前進開始
+            if (_touchTime > DELAY_JUMP_FRAME){
+                Move(); // 移動
+            }
         }
 
-        _oldGroundValue = _checkGround.IsGround.Value;
+        // 離されたとき
+        if (Input.GetMouseButtonUp(0)){
+            // タッチ終了座標
+            _touchEndPosY = Input.mousePosition.y;
+
+            // 移動していない場合ジャンプを実行
+            if (_advanceFlg == false){
+                StartCoroutine("Jump");
+            }
+
+            // 指が離れた時に前進移動を停止
+            if ((_advanceFlg == true /*&& _checkGround.IsGround.Value*/) == true) { 
+                MoveStop(); // 移動停止
+            }
+
+            // スワイプしている場合
+            if (ACCEPT_ABLE_VALUE < Math.Abs(_touchEndPosY) - Mathf.Abs(_touchStartPosY)){
+                _jumpMoveFlg = true;
+                JumpMove();
+            }
+            // タッチ時間をリセット
+            _touchTime = 0;
+        }
     }
+
+
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    IEnumerator Jump()
+    {
+        _jumpFlg = true;
+        // 1フレーム待つ
+        yield return null;
+        _jumpFlg = false;
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    void Move()
+    {
+        _advanceFlg = true;
+    }
+
+    /// <summary>
+    /// 移動停止
+    /// </summary>
+    void MoveStop()
+    {
+        _advanceFlg = false;
+    }
+
+    /// <summary>
+    /// ジャンプしながら移動
+    /// </summary>
+    void JumpMove()
+    {
+        if (_jumpMoveFlg == true)
+        {
+            StartCoroutine("Jump");
+
+            _advanceFlg = true;
+        }
+    }
+
+    //foreach (Touch t in Input.touches)
+    //{
+    //    switch (t.phase)
+    //    {
+    //case TouchPhase.Began:
+    //    // タッチ開始座標を代入
+    //    _touchStartPosY = t.position.y;
+
+    //    Debug.Log("Begin");
+    //    break;
+    //case TouchPhase.Stationary:
+    //// タッチ時間計測を開始
+    //_touchTime++;
+    //Debug.Log("Stationaly");
+
+    // タッチ時間が一定フレームを超えたら前進開始
+    //                if (_touchTime > _delayFrame /*&& _checkGround.IsGround.Value == true*/)
+    //                {
+    //                    Move();
+    //                    //_advanceFlg = true;
+    //                }
+    //                break;
+    //            case TouchPhase.Ended:
+    //                // タッチ終了座標を代入
+    //                _touchEndPosY = t.position.y;
+
+    //                Debug.Log("StartPos" + _touchStartPosY);
+
+    //                Debug.Log("EndPos" + _touchEndPosY);
+
+    //                // 移動していない場合ジャンプを実行
+    //                if (_advanceFlg == false)
+    //                {
+    //                    Jump();
+    //                    //_jumpFlg = true;
+    //                    //Observable.NextFrame().Subscribe(_ =>
+    //                    //{
+    //                    //    _jumpFlg = false;
+    //                    //});
+    //                }
+
+    //                // 指が離れた時に前進移動を停止
+    //                if ((_advanceFlg == true && _checkGround.IsGround.Value) == true)
+    //                {
+    //                    _advanceFlg = false;
+    //                }
+
+    //                if (30 < Math.Abs(_touchEndPosY) - Mathf.Abs(_touchStartPosY))
+    //                {
+    //                    // スワイプしている場合
+    //                    //_advanceFlg = true;
+    //                    if ((_checkGround.IsGround.Value) == true)
+    //                    {
+    //                        JumpMove();
+    //                        //transform.DOMoveZ(transform.position.z + _adPower, _jumpTime);
+    //                        //transform.DOMoveY(transform.position.y + 5, _jumpTime);
+    //                    }
+    //                }
+    //                // タッチ時間をリセット
+    //                _touchTime = 0;
+    //                break;
+    //        }
+    //    }
+    //    if (_checkGround.IsGround.Value != _oldGroundValue)
+    //    {
+    //        _advanceFlg = false;
+    //    }
+    //    _oldGroundValue = _checkGround.IsGround.Value;
+    //}
 
 }
