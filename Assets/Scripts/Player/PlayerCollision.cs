879cf0b671c0b3dc5ebac6103a88f5fde39adf27
hetima333 <hetima333@gmail.com>
2017-06-05 15:51:12 +0900

    [WIP]当たり判定の修正

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -1,7 +1,7 @@
 ﻿// ---------------------------------------
 // Brief : プレイヤーの当たり判定
 // 
-// Date  : 2014/04/24
+// Date  : 2016/04/24
 // 
 // Author: Y.Watanabe
 // ---------------------------------------
@@ -9,10 +9,105 @@
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
-using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerCollision : MonoBehaviour
 {
+	[SerializeField, Range(0, 10), Tooltip("はさまれた判定を出す最短の距離")]
+	private float m_crossDistance;
+
+	// オブジェクトと接触点
+	// private Dictionary<GameObject, Vector3> m_contacts = new Dictionary<GameObject, Vector3>();
+
+	private List<GameObject> m_contactsObjects = new List<GameObject>();
+
+	private List<Vector3> m_contactPoints = new List<Vector3>();
+
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	private void Start ()
+	{
+		var core = GetComponent<PlayerCore>();
+
+		this.OnCollisionStayAsObservable()
+			.Subscribe(x =>
+			{
+				foreach (var contact in x.contacts)
+				{
+					// 自分自身の判定のみを取る
+					if(contact.thisCollider == GetComponent<Collider>())
+					{
+						var hit = contact.otherCollider.gameObject;
+						// 既に接触しているか？
+						if (m_contactsObjects.Contains(hit) == false)
+						//if(m_contacts.ContainsKey(contact.otherCollider.gameObject) == false)
+						{
+							Debug.Log(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+
+							// 未登録のオブジェクトは登録する
+							m_contactsObjects.Add(hit);
+							m_contactPoints.Add(contact.point);
+						}
+						else
+						{
+							m_contactPoints[m_contactsObjects.IndexOf(hit)] = contact.point;
+						}
+					}
+				}
+			});
+
+		this.OnCollisionExitAsObservable()
+			.Subscribe(x =>
+			{
+				List<GameObject> keyList = new List<GameObject>(m_contactsObjects);
+				foreach (var key in keyList)
+				{
+					// 判定用フラグ
+					bool flg = false;
+
+					// オブジェクトがまだ接しているかを判定する
+					foreach (var contact in x.contacts)
+					{
+						if (contact.thisCollider.gameObject == key)
+							flg = true;
+						else
+							flg = false;
+					}
+
+					// 接していない場合は削除する
+					if (flg == false)
+					{
+						Debug.Log("delete " + key.name);
+						int index = m_contactsObjects.IndexOf(key);
+						m_contactsObjects.RemoveAt(index);
+						m_contactPoints.RemoveAt(index);
+					}
+						
+				}
+			});
+
+		this.UpdateAsObservable()
+			// 2つ以上のオブジェクトに接している
+			.Where(_ => m_contactPoints.Count > 1)
+			.Subscribe(_ =>
+			{
+				for (int i = 0; i < m_contactPoints.Count - 1; i++)
+				{
+					float distance = Vector3.Distance(m_contactPoints[i], m_contactPoints[i + 1]);
+
+					// 2点間の距離が規定の距離より大きい場合は挟まれた判定を出す
+					if (distance <= 1.1f && distance > 0.9f/*/|| distance > m_crossDistance/*/)
+					{
+						Debug.Log("distance " + distance);
+						core.IsDead.Value = true;
+					}
+				}
+			});
+	}
+
+	/*/
 	private Collider[] m_colliders;
 
 	// 衝突相手
@@ -93,4 +188,5 @@ public class PlayerCollision : MonoBehaviour
 
 		//IsHit.Subscribe(x => core.PlayerControllable.SetValueAndForceNotify(x));
 	}
+	/*/
 }
