2549bf51cc2d3b543098ddd2c0c2eb2d338960db
TSITCL\s162034 <elitesr2110@gmail.com>
2017-06-12 14:01:06 +0900

    BGMの整理

---
+++ Assets/Scripts/Sounds/AudioTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioManager.Instance.AudioSet("BGM", "MP3\\bgm");
+        AudioManager.Instance.Play("BGM");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
