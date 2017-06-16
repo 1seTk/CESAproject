19c22b67ff2eb64dc8403d2233c75d4ee5de4c1d
shun9 <hanashun9@gmail.com>
2017-06-16 15:02:57 +0900

    Stage3の参照が切れていたのを修正 Timer更新

--- Assets/Scripts/Clear/Timer.cs
+++ Assets/Scripts/Clear/Timer.cs
@@ -2,23 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-
     [SerializeField, Tooltip("Player")]
-    private GameObject  m_player;
+    private GameObject m_player;
 
-    private PlayerCore  m_playerCore;
+    //プレイヤー情報取得用
+    private PlayerCore m_playerCore;
     private PlayerEnter m_playerEnter;
+
+    //時間描画用
+    private Text m_text;
 
     // 経過時間
     private float m_elapsedTime = 0.0f;
+
+    //クリア処理をしたかどうか
+    private bool m_isClear = false;
+
+    //ベストタイムを記録するキー
+    private string m_recordKey;
 
     void Start ()
     {
         m_playerCore = m_player.GetComponent<PlayerCore>();
         m_playerEnter = m_player.GetComponent<PlayerEnter>();
+        m_text = GetComponent<Text>();
+        m_recordKey = YamagenLib.SelectManager.instance.GetSelectStage().ToString() + "Time";
+
+        Debug.Log(YamagenLib.SelectManager.instance.GetSelectStage().ToString());
     }
 
 
@@ -26,32 +40,35 @@ public class Timer : MonoBehaviour
     void Update ()
     {
         //時間描画
-        DrawTime();
+        //m_text.text = ConvertStringTime(m_elapsedTime);
+        m_text.text = ConvertStringTime(PlayerPrefs.GetFloat(m_recordKey, float.MaxValue));
 
         //プレイヤーが登場するまでカウントしない
         if (!(m_playerEnter.IsPlayerEnter)) return;
 
-        //プレイヤーが生きている間カウントする
+        //プレイヤーが生きていてゴールしていない間カウントする
         if (!(m_playerCore.IsDead.Value))
         {
-            m_elapsedTime += Time.deltaTime;
-        }
-
-	}
-
-    IEnumerator WaitStartPlayer()
-    {
-        // プレイヤーが登場し終えるまで待機
-        while (m_playerEnter.IsPlayerEnter == false)
-        {
-            yield return new WaitForEndOfFrame();
+            if ((ShunLib.ClearDirection.instance.isClear() == false))
+            {
+                m_elapsedTime += Time.deltaTime;
+            }
+            else
+            {
+                //クリア後の処理をする
+                if (!m_isClear)
+                {
+                    RecordTime();
+                    m_isClear = true;
+                }
+            }
         }
     }
 
     /// <summary>
     /// 経過時間をリセット
     /// </summary>
-    void Reset()
+    public void Reset()
     {
         m_elapsedTime = 0.0f;
     }
@@ -61,23 +78,36 @@ public class Timer : MonoBehaviour
     /// 経過時間を取得
     /// </summary>
     /// <returns>経過時間</returns>
-    float GetTime()
+    public float GetTime()
     {
         return m_elapsedTime;
     }
 
     /// <summary>
-    /// タイムを表示
+    /// タイムを文字列に変換して返す
     /// </summary>
-    void DrawTime()
+    static public string ConvertStringTime(float time)
     {
-        float time = m_elapsedTime;
-
         //分・秒・ミリ秒
         int minute = (int)time / 60;
         int second = (int)time % 60;
         int millisecond = (int)((time - (int)time) * 100) ;
 
-        GetComponent<Text>().text = minute.ToString().PadLeft(2,'0') +":"+ second.ToString().PadLeft(2, '0') + ":"+ millisecond.ToString().PadLeft(2, '0');
+        return (minute.ToString().PadLeft(2,'0') +":"+ second.ToString().PadLeft(2, '0') + ":"+ millisecond.ToString().PadLeft(2, '0'));
+    }
+
+    /// <summary>
+    /// ベストタイムを更新していたら記録
+    /// </summary>
+    void RecordTime()
+    {
+        float bestTime = PlayerPrefs.GetFloat(m_recordKey, float.MaxValue);
+
+        //ベストタイムより速かったら記録
+        if (bestTime > m_elapsedTime)
+        {
+            Debug.Log("Record");
+            PlayerPrefs.SetFloat(m_recordKey, m_elapsedTime);
+        }
     }
 }
